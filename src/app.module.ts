b3a46d309128fbf9484bf33b8cc1f1ef7db77b00
lexdier <saviolexdier@gmail.com>
2023-07-14 17:52:07 -0300

    feat: set db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {MongooseModule} from "@nestjs/mongoose";
+import {NewsSchema} from "./news/news.schema";
+import {NewsController} from "./news/news.controller";
+import {NewsService} from "./news/news.service";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [MongooseModule.forRoot(
+        'mongodb+srv://saviolexdier:tMpPLUw0em84mNfx@news.vbinqhb.mongodb.net/'
+    ), MongooseModule.forFeature([{name: 'News', schema: NewsSchema}])],
+    controllers: [AppController, NewsController],
+    providers: [AppService, NewsService],
 })
-export class AppModule {}
+export class AppModule {
+}
