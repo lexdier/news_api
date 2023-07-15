45cf6562a06602fac2cec2a3707fb02e31c5b49e
lexdier <saviolexdier@gmail.com>
2023-07-15 14:18:49 -0300

    feat: create news validator

---
+++ src/validators/news.validator.ts
@@ -0,0 +1,45 @@
+import {IsOptional, IsString} from "class-validator";
+
+export class CreateNewsValidator {
+
+    @IsString()
+    readonly title: string;
+
+    @IsString()
+    readonly description: string;
+
+    @IsString()
+    readonly url: string;
+
+    @IsString()
+    readonly author: string;
+
+    @IsString()
+    readonly source: string;
+
+    @IsString()
+    readonly tags: string[];
+}
+
+export class UpdateNewsValidator {
+
+    @IsOptional()
+    @IsString()
+    title: string;
+
+    @IsOptional()
+    @IsString()
+    description: string;
+
+    @IsOptional()
+    @IsString()
+    url: string;
+
+    @IsOptional()
+    @IsString()
+    author: string;
+
+    @IsOptional()
+    @IsString()
+    tags: string[];
+}
